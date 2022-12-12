1dd932c6f3f0030d07b8d0295b28da5af45a91c9
divyanshchahar <divyanshchahar@outlook.com>
2022-12-12 22:46:31 +0530

    added componenet to render menu button

---
+++ src/components/MenuButton.js
@@ -0,0 +1,18 @@
+import MenuIcon from "../assets/menu_icon.svg";
+
+/**
+ *
+ * @param {array} args - array comprised of `stateVar` and `stateFunc` i.e. state variable and state function respectively
+ * @returns
+ */
+
+function MenuButton(args) {
+  const [stateVar, stateFunc] = args.params;
+  return (
+    <div className="menu-button" onClick={() => stateFunc(!stateVar)}>
+      <img src={MenuIcon} alt="" />
+    </div>
+  );
+}
+
+export default MenuButton;
