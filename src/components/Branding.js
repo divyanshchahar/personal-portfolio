6c6067bb44cf5e8e9928fe08c05a4d611ab2c5c9
divyanshchahar <divyanshchahar@outlook.com>
2022-12-12 22:44:14 +0530

    added componenet to render branding

---
+++ src/components/Branding.js
@@ -0,0 +1,10 @@
+/**
+ * Function to render branding
+ * @returns {HTML} - HTML to render banding
+ */
+
+function Branding() {
+  return <div className="branding">Divyansh</div>;
+}
+
+export default Branding;
