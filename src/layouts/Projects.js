65f0b3e1b26fe8e7d4d25b98575d11a665d96a00
divyanshchahar <divyanshchahar@outlook.com>
2023-09-15 15:46:36 +0530

    closes #35

--- src/layouts/Projects.js
+++ src/layouts/Projects.js
@@ -12,21 +12,21 @@ function Projects() {
       <h1 className="section-heading">Projects</h1>
       <div className="tabular-medium">
         <a
-          href="https://github.com/divyanshchahar/e_store"
+          href="https://github.com/divyanshchahar/virtual_store"
           target="_blank"
           rel="noreferrer"
         >
           <div className="project-container">
             <img src={Folder} alt="folder icon" />
-            <h2 className="sub-section-heading">E-Store</h2>
+            <h2 className="sub-section-heading">E-Commerce App</h2>
             <p>
-              This was my first serious react project, I intended to make a
-              fully functional e-store but I soon realised it can't be done
-              without knowing some backend skills. Although I improvised with
-              json-server for the time being, I plan to refactor this project
-              and make it my first MERN app. Since it was my first big react
-              project, it has been refactored multiple times and the code looks
-              a little bit messy.
+              E-commerce store is a cutting-edge web application developed using
+              the MERN (MongoDB, Express.js, React, Node.js) stack and organized
+              as a yarn monorepo for efficient code management and scalability.
+              One of the standout features of this project is the robust
+              authorization and authentication system, which draws inspiration
+              from the OAuth 2.0 protocol and has been entirely developed
+              in-house.
             </p>
           </div>
         </a>
