3f72ac953172efbf014837bc2e5d629e20872e79
divyanshchahar <divyanshchahar@outlook.com>
2024-06-17 17:09:16 +0530

    integrated new component

--- src/ui/components/FrontMatterLinks.js
+++ src/ui/components/FrontMatterLinks.js
@@ -1,7 +1,8 @@
-import GithubIcon from '../../assets/github.svg';
-import HashnodeIcon from '../../assets/hashnode.svg';
-import LinkedInIcon from '../../assets/linkedin.svg';
-import TwitterIcon from '../../assets/twitter.svg';
+import GithubIcon from '../../assets/github_icon.svg';
+import HashnodeIcon from '../../assets/hashnode_icon.svg';
+import LinkedInIcon from '../../assets/linkedin_icon.svg';
+import TwitterIcon from '../../assets/twitter_icon.svg';
+import IconLinkComponent from './IconLinkComponent';
 
 /**
  * Componenet to render links(images) on the front matter
@@ -10,37 +11,29 @@ import TwitterIcon from '../../assets/twitter.svg';
 function FrontMatterLinks() {
 	return (
 		<div className="front-matter-links">
-			<a
-				href="https://www.linkedin.com/in/divyanshchahar/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<img src={LinkedInIcon} alt="linked icon" />
-			</a>
+			<IconLinkComponent
+				icon={LinkedInIcon}
+				linkAddress="https://www.linkedin.com/in/divyanshchahar/"
+				altTag="linkedin"
+			/>
 
-			<a
-				href="https://twitter.com/DivyanshChahar"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<img src={TwitterIcon} alt="twitter icon" />
-			</a>
+			<IconLinkComponent
+				icon={TwitterIcon}
+				linkAddress="https://twitter.com/DivyanshChahar"
+				altTag="twitter"
+			/>
 
-			<a
-				href="https://github.com/divyanshchahar"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<img src={GithubIcon} alt="github" />
-			</a>
+			<IconLinkComponent
+				icon={GithubIcon}
+				linkAddress="https://github.com/divyanshchahar"
+				altTag="github"
+			/>
 
-			<a
-				href="https://divyanshchahar.hashnode.dev/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<img src={HashnodeIcon} alt="" />
-			</a>
+			<IconLinkComponent
+				icon={HashnodeIcon}
+				linkAddress="https://divyanshchahar.hashnode.dev/"
+				altTag="hashnode"
+			/>
 		</div>
 	);
 }
