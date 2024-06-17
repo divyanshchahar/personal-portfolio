0ce2158892263c9683a66e4cf116b338e5d2bfb5
divyanshchahar <divyanshchahar@outlook.com>
2024-06-17 15:56:39 +0530

    updated styling

--- src/ui/components/IconButtonComponent.js
+++ src/ui/components/IconButtonComponent.js
@@ -1,14 +1,16 @@
+import styles from './IconButtonComponent.module.css';
+
 /**
  * Function to render menu button
  * @param {array} args - array comprised of `stateVar` and `stateFunc` i.e. state variable and state function respectively
  * @returns
  */
 
-function MenuButton({ icon, clickHandler }) {
+function MenuButton({ icon, clickHandler = () => {} }) {
 	return (
-		<div className="menu-button" onClick={clickHandler}>
+		<button className={styles.container} onClick={clickHandler}>
 			<img src={icon} alt="" />
-		</div>
+		</button>
 	);
 }
 
